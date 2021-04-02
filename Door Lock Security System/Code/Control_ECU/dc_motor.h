b45086e241c2541dc2b236afc130b66483e1d774
Ahmed Hassan <ahmed.hassan9575@gmail.com>
2021-04-02 21:19:44 +0200

    Update dc_motor.h

--- Door Lock Security System/Code/Control_ECU/dc_motor.h
+++ Door Lock Security System/Code/Control_ECU/dc_motor.h
@@ -56,15 +56,15 @@ void DCMOTOR_init(void);
  *
  * [Description]: Function to force the DC MOTOR to rotate Clockwise
  *
- * [Args]:			None
+ * [Args]:		None
  * 
- * [in]			   None
+ * [in]			 None
  *
- * [out]		  	None
+ * [out]			None
  *
- * [in/out]		 	 None
+ * [in/out]		  None
  *
- * [Returns]:      None
+ * [Returns]:     None
  *******************************************************************************/
 void DCMOTOR_rotateClockwise(void);
 
