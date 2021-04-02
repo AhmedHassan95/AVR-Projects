9d55ab603407623b74c03852f467811925c84993
Ahmed Hassan <ahmed.hassan9575@gmail.com>
2021-04-02 20:18:25 +0200

    Update application_functions.h

--- Door Lock Security System/Code/Control_ECU/application_functions.h
+++ Door Lock Security System/Code/Control_ECU/application_functions.h
@@ -45,8 +45,8 @@
 #define NO_ATTEMPT		    0		/* There are not attempt yet */
 #define MAXIMUM_ATTEMPTS            3		/* Maximum wrong attempts to enter the system */
 
-#define  INITIAL_VALUE            0       /* Initial value to start the timer count from it */
-#define  FIRST_TICK               1		/* First time the interrupt occurred */
+#define  INITIAL_VALUE             0       /* Initial value to start the timer count from it */
+#define  FIRST_TICK                1		/* First time the interrupt occurred */
 
 #define  UNLOCKED                   0       /* Unlock the system */
 #define  OPENED                     1		/* System Opened */
