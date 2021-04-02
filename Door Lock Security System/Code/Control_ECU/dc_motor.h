4a3aa811b9bf001590d525c680600800a9065210
Ahmed Hassan <ahmed.hassan9575@gmail.com>
2021-04-02 21:28:25 +0200

    Update dc_motor.h

--- Door Lock Security System/Code/Control_ECU/dc_motor.h
+++ Door Lock Security System/Code/Control_ECU/dc_motor.h
@@ -92,11 +92,11 @@ void DCMOTOR_rotateAntiClockwise(void);
  *
  * [Args]:	  None
  *
- * [in]		 None
+ * [in]		  None
  *
- * [out]	 None
+ * [out]	  None
  *
- * [in/out]	 None
+ * [in/out]	  None
  *
  * [Returns]:     None
  *******************************************************************************/
