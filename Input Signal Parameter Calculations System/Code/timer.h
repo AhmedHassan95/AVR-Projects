ed594b0efe47326487b4dd45d0ddc1c16ebd8fec
Ahmed Hassan <ahmed.hassan9575@gmail.com>
2021-04-02 15:50:30 +0200

    Update timer.h

--- Input Signal Parameter Calculations System/Code/timer.h
+++ Input Signal Parameter Calculations System/Code/timer.h
@@ -57,17 +57,17 @@ void TIMER1_clearTimerValue(void);
  * [Function Name]: TIMER1_getTimerValue
  *
  * [Description]:   Function to get the Timer1 Value, to store it in the global variables,
- * 			then use these variables to calculate the Duty Cycle of the signal
+ * 		then use these variables to calculate the Duty Cycle of the signal
  *
- * [Args]:	None
+ * [Args]:	 None
  *
- * [in]		None
+ * [in]		 None
  *
  * [out]	 Unsigned Character
  *
- * [in/out]	None
+ * [in/out]	 None
  *
- * [Returns]:       The TIMER1 register value
+ * [Returns]:      The TIMER1 register value
  ********************************************************************************/
 uint16 TIMER1_getTimerValue(void);
 
