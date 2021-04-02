82c624b592392ff4c12e738ab4081e38efb2f3bc
Ahmed Hassan <ahmed.hassan9575@gmail.com>
2021-04-03 01:26:12 +0200

    Update application_functions.h

--- Door Lock Security System/Code/HMI_ECU/application_functions.h
+++ Door Lock Security System/Code/HMI_ECU/application_functions.h
@@ -75,11 +75,11 @@ extern uint8 g_wrongAttempts;		 /* Global variable to store the number of wrong 
  * [Function Name]: APP_setNewPassword
  *
  * [Description]: Function to set the new user password
- * 		- Display a message, if this is the first time to login to the system
+ * 		 - Display a message, if this is the first time to login to the system
  * 		   - Take new password from the user for two times
- * 		 - Compare between these two passwords
+ * 		  - Compare between these two passwords
  * 		  - If two passwords are identical, send password to the Control ECU to be
- * 		  	 stored in the EEPROM
+ * 		  	stored in the EEPROM
  * 		  - If two passwords are not identical, repeat these steps again
  *
  * [Args]:	 a_systemStatus
