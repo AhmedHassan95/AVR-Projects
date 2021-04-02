e13c16836fa10cb8bc22e605c580cc0f2ee2ce03
Ahmed Hassan <ahmed.hassan9575@gmail.com>
2021-04-02 15:43:05 +0200

    Update timer.h

--- Input Signal Parameter Calculations System/Code/timer.h
+++ Input Signal Parameter Calculations System/Code/timer.h
@@ -26,11 +26,11 @@
  *
  * [Args]:	   None
  *
- * [in]		  None
+ * [in]		   None
  *
- * [out]	  None
+ * [out]	   None
  *
- * [in/out]	  None
+ * [in/out]	   None
  *
  * [Returns]:       None
  ********************************************************************************/
