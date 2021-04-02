314327d877052c6265f203e2017dcdf65ff28be7
Ahmed Hassan <ahmed.hassan9575@gmail.com>
2021-04-02 21:16:01 +0200

    Update dc_motor.h

--- Door Lock Security System/Code/Control_ECU/dc_motor.h
+++ Door Lock Security System/Code/Control_ECU/dc_motor.h
@@ -35,17 +35,17 @@
 /*******************************************************************************
  * [Function Name]: DCMOTOR_init
  *
- * [Description]: 	Function to Initialize the DC MOTOR Driver
- *					- Decide DC MOTOR pins direction
- * 					- Turn off the DC MOTOR at the beginning as the initial state
+ * [Description]: Function to Initialize the DC MOTOR Driver
+ *				- Decide DC MOTOR pins direction
+ * 				- Turn off the DC MOTOR at the beginning as the initial state
  *
- * [Args]:			None
+ * [Args]:		None
  *
- * [in]			  	None
+ * [in]			  None
  *
- * [out]		  	None
+ * [out]		  None
  *
- * [in/out]		 	None
+ * [in/out]		 None
  *
  * [Returns]:       None
  *******************************************************************************/
