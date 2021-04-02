23d504e73715ce258533458b9bc3f8d5457d49fb
Ahmed Hassan <ahmed.hassan9575@gmail.com>
2021-04-02 05:41:11 +0200

    Update interrupt.c

--- Monitoring Motor speed according to Temperature Sensor/Code/interrupt.c
+++ Monitoring Motor speed according to Temperature Sensor/Code/interrupt.c
@@ -18,7 +18,7 @@
 ISR(INT1_vect)
 {
 	/*
-	 *	Toggle pin 0 & pin 1 in PORTB in order to revert the diction of the DC Motor
+	 * Toggle pin 0 & pin 1 in PORTB in order to revert the diction of the DC Motor
 	 */
 	TOGGLE_BIT(PORTB,PB0);
 	TOGGLE_BIT(PORTB,PB1);
@@ -32,20 +32,20 @@ ISR(INT1_vect)
  *
  * [Description]:   Function to enable and configure External INT1
  *
- * [Args]:			None
+ * [Args]:	    None
  *
- * [in]			  	None
+ * [in]	            None
  *
- * [out]		  	None
+ * [out]	    None
  *
- * [in/out]		 	None
+ * [in/out]	    None
  *
  * [Returns]:       None
  *******************************************************************************/
 void INT1_init(void)
 {
 	CLEAR_BIT(DDRD,PD3);	/* Configure INT1/PD3 as input pin */
-	SET_BIT(GICR,INT1);		/* Enable external interrupt pin INT1 */
+	SET_BIT(GICR,INT1);	/* Enable external interrupt pin INT1 */
 	/* Trigger INT1 with the Raising edge */
 	SET_BIT(MCUCR,ISC10);
 	SET_BIT(MCUCR,ISC11);
