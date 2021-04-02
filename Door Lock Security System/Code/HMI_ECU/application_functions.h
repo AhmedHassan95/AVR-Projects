500ee9d4a07341ec58a395cd7fa088257221fdb5
Ahmed Hassan <ahmed.hassan9575@gmail.com>
2021-04-03 01:52:20 +0200

    Update application_functions.h

--- Door Lock Security System/Code/HMI_ECU/application_functions.h
+++ Door Lock Security System/Code/HMI_ECU/application_functions.h
@@ -153,20 +153,20 @@ void APP_sendPassword(uint8 * Password_Ptr);
  * [Description]: Function that is responsible for doing the following steps:
  *                 - Increment the number of wrong attempts
  *                 - Display the message "INCORRECT PASSWORD"
- *					 - Check the number of wrong attempts:
- *					1- If it is not equal to MAXIMUM NUMBER OF ATTEMPTS, return
- *						  from the function to get the password again from the user
- *					2- If it is equal to MAXIMUM NUMBER OF ATTEMPTS, count the
- *						  alarm time and hold the system for (15 Seconds), then return
- *						  back to the MAIN LOOP again
+ *			- Check the number of wrong attempts:
+ *				1- If it is not equal to MAXIMUM NUMBER OF ATTEMPTS, return
+ *						 from the function to get the password again from the user
+ *				2- If it is equal to MAXIMUM NUMBER OF ATTEMPTS, count the
+ *						 alarm time and hold the system for (15 Seconds), then return
+ *						 back to the MAIN LOOP again
  *
  * [Args]:		None
  *
- * [in]			  None
+ * [in]		 None
  *
- * [out]		  None
+ * [out]	None
  *
- * [in/out]		 None
+ * [in/out]	None
  *
  * [Returns]:       None
  ********************************************************************************/
@@ -179,11 +179,11 @@ void APP_incorrectPassword(void);
  *
  * [Args]:		None
  *
- * [in]			 None
+ * [in]		None
  *
- * [out]		 None
+ * [out]	None
  *
- * [in/out]		None
+ * [in/out]	None
  *
  * [Returns]:    None
  ********************************************************************************/
@@ -196,11 +196,11 @@ void APP_countAlarmTime(void);
  *
  * [Args]:	None
  *
- * [in]			None
+ * [in]	None
  *
- * [out]		 None
+ * [out]	None
  *
- * [in/out]		None
+ * [in/out]	None
  *
  * [Returns]:     None
  ********************************************************************************/
@@ -213,11 +213,11 @@ void APP_countMotorRunTime(void);
  *
  * [Args]: None
  *
- * [in]			None
+ * [in]		None
  *
- * [out]		 None
+ * [out]		None
  *
- * [in/out]		None
+ * [in/out]	None
  *
  * [Returns]:     None
  ********************************************************************************/
