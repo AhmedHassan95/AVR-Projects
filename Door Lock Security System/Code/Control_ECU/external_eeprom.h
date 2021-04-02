469acd78a6cc6776c3a58ec5f9aa5f410cd563a7
Ahmed Hassan <ahmed.hassan9575@gmail.com>
2021-04-02 21:31:53 +0200

    Update external_eeprom.h

--- Door Lock Security System/Code/Control_ECU/external_eeprom.h
+++ Door Lock Security System/Code/Control_ECU/external_eeprom.h
@@ -31,13 +31,13 @@
  *
  * [Description]: Function responsible for initializing EEPROM driver
  *
- * [Args]:		None
+ * [Args]:	None
  *
- * [in]			  None
+ * [in]			 None
  *
- * [out]		  None
+ * [out]		 None
  *
- * [in/out]		 None
+ * [in/out]		None
  *
  * [Returns]:       None
  *******************************************************************************/
