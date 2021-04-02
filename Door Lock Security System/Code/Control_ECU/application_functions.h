462e8d8915eeb1f40d06382d6cb56678d0817fe9
Ahmed Hassan <ahmed.hassan9575@gmail.com>
2021-04-02 23:19:12 +0200

    Update application_functions.h

--- Door Lock Security System/Code/Control_ECU/application_functions.h
+++ Door Lock Security System/Code/Control_ECU/application_functions.h
@@ -98,7 +98,7 @@ uint8 APP_checkPasswordMismatch(uint8 * Password1_Ptr , uint8 * Password2_Ptr);
  *
  * [Description]: Function to receive the new password from the HMI ECU
  *
- * [Args]:   	Password_Ptr
+ * [Args]:   	  Password_Ptr
  *
  * [in]	          Password_Ptr: Pointer to character
  *
