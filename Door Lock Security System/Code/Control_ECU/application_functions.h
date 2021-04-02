ba45a50e598375c0b6f12ae833e5336a5365a9c8
Ahmed Hassan <ahmed.hassan9575@gmail.com>
2021-04-02 20:15:53 +0200

    Update application_functions.h

--- Door Lock Security System/Code/Control_ECU/application_functions.h
+++ Door Lock Security System/Code/Control_ECU/application_functions.h
@@ -48,14 +48,14 @@
 #define  INITIAL_VALUE            0       /* Initial value to start the timer count from it */
 #define  FIRST_TICK               1		/* First time the interrupt occurred */
 
-#define  UNLOCKED                 0       /* Unlock the system */
-#define  OPENED                   1		/* System Opened */
+#define  UNLOCKED                  0       /* Unlock the system */
+#define  OPENED                    1		/* System Opened */
 
-#define MAXIMUM_ALARM_TIME        15      /* Maximum time to turn on the Alarm */
-#define	MAXIMUM_RUNNING_TIME      15		/* Maximum time to turn on the Motor */
-#define MAXIMUM_STOPING_TIME      3		/* Maximum time to turn off the Motor */
+#define MAXIMUM_ALARM_TIME         15      /* Maximum time to turn on the Alarm */
+#define	MAXIMUM_RUNNING_TIME       15		/* Maximum time to turn on the Motor */
+#define MAXIMUM_STOPING_TIME       3		/* Maximum time to turn off the Motor */
 
-#define  COUNT_ONE_SECOND         8000    /* Number be loaded in the timer to count 1S */
+#define  COUNT_ONE_SECOND           8000    /* Number be loaded in the timer to count 1S */
 
 #define CHANGE_PASSWORD     	    43 		/* ACII Code for '+' */
 #define DOOR_OPEN          	    45		/* ACII Code for '-' */
