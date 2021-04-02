b60025f1ce8e49d50ae3e4e1dde3e09fb4c08022
Ahmed Hassan <ahmed.hassan9575@gmail.com>
2021-04-02 05:38:27 +0200

    Update adc.h

--- Monitoring Motor speed according to Temperature Sensor/Code/adc.h
+++ Monitoring Motor speed according to Temperature Sensor/Code/adc.h
@@ -82,11 +82,11 @@ uint16 ADC_readChannel(uint8 channel_num);
  * [Description]:   Function responsible for reading analog data from a certain ADC channel
  * 	              and convert it to digital using the ADC driver
  *
- * [Args]:	 channel_num
+ * [Args]:	channel_num
  *
- * [in]	     channel_num: Unsigned Character to store the channel number in it
+ * [in]	       channel_num: Unsigned Character to store the channel number in it
  *
- * [out]     None
+ * [out]       None
  *
  * [in/out]  	None
  *
