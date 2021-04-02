3e55cbcab1f1b7a53a9d23ca7eb92283bc5ac93a
Ahmed Hassan <ahmed.hassan9575@gmail.com>
2021-04-02 14:28:15 +0200

    Update monitoring_motor_speed.c

--- Monitoring Motor speed according to Temperature Sensor/Code/monitoring_motor_speed.c
+++ Monitoring Motor speed according to Temperature Sensor/Code/monitoring_motor_speed.c
@@ -7,16 +7,16 @@
  * [DATE CREATED] :	Feb 14, 2021
  *
  * [DESCRIPTION] :	This Project is to design a system that control DC Motor speed through PWM
- * 					according to temperature sensor degree that connected through ADC Driver.
+ * 			according to temperature sensor degree that connected through ADC Driver.
  *
- * 					Design Considerations:
+ * 			Design Considerations:
  *
- * 					- MCU : ATmega 16 (F_CPU = 1 MHZ).
- *					- Display Temperature degree on LCD (2 * 16) in PORTC.
- *					- Control Motor speed using PWM [TIMER CTC Mode].
- *					- Use SW connected in (Pin3) in PORTD to revert the Motor Direction[INT0].
- *					- Sensor is connected through the ADC Driver, then Display the result on
- *				          the LCD.
+ * 			- MCU : ATmega 16 (F_CPU = 1 MHZ).
+ *			- Display Temperature degree on LCD (2 * 16) in PORTC.
+ *			- Control Motor speed using PWM [TIMER CTC Mode].
+ *			- Use SW connected in (Pin3) in PORTD to revert the Motor Direction[INT0].
+ *			- Sensor is connected through the ADC Driver, then Display the result on
+ *			  the LCD.
  *
  ***********************************************************************************************/
 
